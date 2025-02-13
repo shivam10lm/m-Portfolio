f9354b74b2b11c8cf971e8872d009f41b329e732
Shivam Gupta <51778763+shivam10lm@users.noreply.github.com>
2025-02-13 16:18:27 -0700

    Update index.js

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -3,5 +3,7 @@ import logo from "./logo.svg";
 import ucal from "./ucal.png";
 import wipro from "./wipro.svg";
 import rubodex from "./rubodex.png";
+import tcAlberta from "./tc.jpeg";
+import reskill from "./reskill.jpeg";
 
-export { hero, logo, ucal, wipro, rubodex };
+export { hero, logo, ucal, wipro, rubodex, tcAlberta, reskill };
